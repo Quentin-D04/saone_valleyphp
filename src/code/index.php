287f0e788e8e13a80a0be431189fd7923e098d8b
Quentin-D04 <quentindeneubourg@gmail.com>
2025-02-03 11:36:50 +0100

    lundi03_02

--- src/code/index.php
+++ src/code/index.php
@@ -11,17 +11,22 @@
     <img src="../assets/img/branche.png" alt="" class="branche">
     <form id="dispo" action="" method="post">
         <label for="Date_one">Date d'arrivée</label>
-        <input type="date" id="Date_one" name="Date_one"><br>
+        <input type="date" id="Date_one" name="Date_one" required><br>
         <label for="Date_two">Date de départ</label>
-        <input type="date" id="Date_two" name="Date_two"><br>
-        <input type="number" id="adultes" name="adultes" placeholder="Adultes"><br>
-        <input type="number" id="enfants" name="enfants" placeholder="Enfants"><br>
-        <input type="number" id="hebergements" name="hebergements" placeholder="hébergements"><br>
+        <input type="date" id="Date_two" name="Date_two" required><br>
+        <input type="number" id="adultes" name="adultes" placeholder="Adultes" required><br>
+        <input type="number" id="enfants" name="enfants" placeholder="Enfants" required><br>
+        <input type="number" id="hebergements" name="hebergements" placeholder="hébergements" required><br>
         <div class="btnform">
             <input type="submit" value="Envoyer" class="btn_form">
         </div>
     </form>
+    >
     <h2 class="h2_accueil">Vivez des moments inoubliables comme nulle part ailleurs !</h2>
     <p class="p_accueil">Sur plus de 5 hectares, au cœur de la Vallée de la Saône, dans un milieu naturel préservé, en bordure d'un plan d'eau privatif et des eaux enchanteresses de la Saône, le Domaine Saône-Valley vous accueille toute l'année pour vivre des vacances uniques.</p>
+    <div class="offres">
+        <h2 class="h2_accueil2">offres Spéciales</h2>
+        <img src="../assets/img/Capture d'écran 2025-02-03 111130.png" alt="offre spéciale" class="offre">
+    </div>
 </main>
 <?php include 'footer.php'; ?>
