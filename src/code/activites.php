c3c5cf6d2bd871103841cb53e8f075688c44dedc
Quentin-D04 <quentindeneubourg@gmail.com>
2025-02-07 09:45:56 +0100

    vendredi06_02

--- src/code/activites.php
+++ src/code/activites.php
@@ -23,6 +23,46 @@
                 <li>80.00€ : 8 heures</li>
             </ul>
         </div>
+        <div class="card">
+            <img src="../assets/img/canoe6.jpg" alt="">
+            <p>Toute l'année la pêche bat son plein au Domaine Saône-Valley<br>Cartes de pêche en vente sur place</p>
+            <ul class="-card2">
+                <li>84.00€ : Anuelle</li>
+                <li>35.00€ : hebdomadaire</li>
+                <li>11.70€ : journalière</li>
+            </ul>
+        </div>
+    </div>
+    <h1 class="h1_activite">Tarifs Acivitées Nautiques</h1>
+    <h2 class="h2_activite"> Posez l'ancre au Domaine Saône-Valley, sur nos nouveaux pontons, et profitez du calme.<br>(Moins de 3 heures : 3.00€)</h2>
+    <div class="fullcard">
+        <div class="card1">
+            <img src="../assets/img/ptbateau.jpg" alt="">
+            <p>Pour une longueur inférieure à 6.99 mètres :
+            <ul class="-card">
+                <li>06.00€ : de 3 à 24h</li>
+                <li>35.00€ : semaine</li>
+                <li>140.00€ : mois</li>
+            </ul>
+        </div>
+        <div class="card">
+            <img src="../assets/img/mbateau.jpg" alt="">
+            <p>Pour une longueur de 7 mètres à 15.99 mètres :</p>
+            <ul class="-card">
+                <li>30.00€ : 2 heures</li>
+                <li>45.00€ : 4 heures</li>
+                <li>80.00€ : 8 heures</li>
+            </ul>
+        </div>
+        <div class="card">
+            <img src="../assets/img/gbateau.jpg" alt="">
+            <p>Toute l'année la pêche bat son plein au Domaine Saône-Valley<br>Cartes de pêche en vente sur place</p>
+            <ul class="-card2">
+                <li>84.00€ : Anuelle</li>
+                <li>35.00€ : hebdomadaire</li>
+                <li>11.70€ : journalière</li>
+            </ul>
+        </div>
     </div>
 
 
